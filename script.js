75fafeda2da514e90d9c69b152447688df4a785a
jonathankeuls <jonathan.keuls@skola.botkyrka.se>
2020-09-28 14:20:29 +0200

    Ändringar och nya funktioner

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 const button = document.getElementById("theButton")
 
-function popup(){
-    alert("Hello")
+function popupOnclick(){
+    alert("Thanks")
+    console.log("Thanks")
+}
+
+function popupOnmouseover(){
+alert("Click!")
+console.log("Click")
 }
 
 
 
-button.onclick = popup
+button.onclick = popupOnclick
+button.onmouseover = popupOnmouseover
